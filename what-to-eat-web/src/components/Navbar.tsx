ff4fd1de27705ad788a8becd12ee72c70d69923a
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-13 17:15:33 +0300

    Small adjustments

--- what-to-eat-web/src/components/Navbar.tsx
+++ what-to-eat-web/src/components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     const [searchActive, setSearchActive] = useState(false)
 
     return (
-        <div className="flex flex-row w-full justify-between items-center h-20 px-5" style={{justifyContent: searchActive ? 'space-between' : 'end'}}>
-            <h1 className="text-4xl font-extrabold title-text" style={{display: searchActive ? 'block' : 'none'}}>Filters</h1>
+        <div className="flex flex-row w-full justify-between items-center h-20 px-5">
+            <h1 className="text-4xl font-extrabold title-text">{searchActive ? 'Filters' : 'What to eat?' }</h1>
             <div onClick={() => setSearchActive(!searchActive)} className="bg-black cursor-pointer h-12 flex w-12 rounded-xl">
                 <img src={searchActive ? closeIcon : searchIcon} className="h-8 m-auto" alt="" />
             </div>
