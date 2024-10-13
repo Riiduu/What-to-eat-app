dc974cd442aca5b7f1df656e1d069b79c84309ad
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-13 17:25:45 +0300

    Another slight adjustment

--- what-to-eat-web/src/components/RandomPick.tsx
+++ what-to-eat-web/src/components/RandomPick.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react"
+//import { useState } from "react"
 
 const RandomPick = () => {
     const randomFood = 'https://hungrybynature.com/wp-content/uploads/2017/09/pinch-of-yum-workshop-19.jpg';
 
   return (
-    <div className="flex justify-center my-6 mx-5">
+    <div className="flex justify-center h-full my-6 cursor-pointer hover:scale-[1.01] duration-200  mx-5">
         {/* Card Container */}
-        <div className="relative w-full max-w-md bg-white rounded-3xl shadow-2xl overflow-hidden">
+        <div className="relative w-full max-w-md bg-white rounded-3xl shadow-md hover:shadow-2xl overflow-hidden">
             {/* Image */}
             <img src={randomFood} alt="Random Food" className="w-full h-full object-cover" />
 
