8ba8d3774ab43486064174ddcf9c83619c590e42
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-13 17:13:00 +0300

    I'm an idiot😅

--- what-to-eat-web/src/components/Navbar.tsx
+++ what-to-eat-web/src/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
 
     return (
         <div className="flex flex-row w-full justify-between items-center h-20 px-5" style={{justifyContent: searchActive ? 'space-between' : 'end'}}>
-            <h1 className="text-4xl font-extrabold title-text hidden" style={{display: searchActive ? 'block' : 'hidden'}}>Filters</h1>
-            <div className="bg-black h-12 flex w-12 rounded-xl">
+            <h1 className="text-4xl font-extrabold title-text" style={{display: searchActive ? 'block' : 'none'}}>Filters</h1>
+            <div onClick={() => setSearchActive(!searchActive)} className="bg-black cursor-pointer h-12 flex w-12 rounded-xl">
                 <img src={searchActive ? closeIcon : searchIcon} className="h-8 m-auto" alt="" />
             </div>
         </div>
