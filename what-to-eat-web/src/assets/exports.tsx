0cdd3aeb2b20748a44c804f7cee9123b0d08cf9b
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-13 17:44:16 +0300

    more icons babyy

--- what-to-eat-web/src/assets/exports.tsx
+++ what-to-eat-web/src/assets/exports.tsx
@@ -2,10 +2,18 @@ import menuIcon from './menu.png';
 import searchIcon from './search.png';
 import closeIcon from './close.png'
 import randomFood from './random-food-img.png'
+import discoverIcon from './discover.png';
+import bookmarkIcon from './bookmark.png';
+import bookmarksIcon from './bookmarks.png';
+import settingsIcon from './settings.png';
 
 export {
     menuIcon, 
     searchIcon,
     closeIcon,
-    randomFood
+    randomFood,
+    discoverIcon,
+    bookmarkIcon,
+    bookmarksIcon,
+    settingsIcon
 }; 
