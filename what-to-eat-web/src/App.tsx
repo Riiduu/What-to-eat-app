a59fbaaa73d455081d17a2a7214401c35b8a65ca
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-14 16:57:00 +0300

    Created a bottom navbar

--- what-to-eat-web/src/App.tsx
+++ what-to-eat-web/src/App.tsx
@@ -3,11 +3,13 @@ import RandomPick from "./components/RandomPick";
 
 function App() {
   return (
-    <div className="bg-gray-200 h-screen">
+    <div className="bg-gray-200 h-screen relative">
       <div className="flex flex-col max-w-md mx-auto bg-white h-screen">
         <Navbar />
         <RandomPick />
+        <div className="h-20">
 
+        </div>
       </div>
       
       
