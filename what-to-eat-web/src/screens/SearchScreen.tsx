a4b8cd91e9eaa2bb980dbca23004159820c8a3a1
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-14 20:07:25 +0300

    improved ingredient items ux and reduced vertical margin for search sections

--- what-to-eat-web/src/screens/SearchScreen.tsx
+++ what-to-eat-web/src/screens/SearchScreen.tsx
@@ -7,18 +7,20 @@ const SearchScreen = () => {
     const [inputActive, setInputActive] = useState(false);
 
     const inputRef = useRef("");
+    const itemRef = useRef();
 
+    // Ingredient item component
     const IngredientItem = (props: any) => {
-        const removeIngredient = () => {            
+        const removeIngredient = () => {                         
             setIngredientsList(ingredientsList.filter(item => item !== props.item));
-
         }
         
         return (
-            <a onClick={removeIngredient} className="bg-blue-500 text-white m-1 px-3 py-2 rounded-2xl font-medium title-text">{props.item}</a>
+            <a onClick={removeIngredient} ref={itemRef} className="bg-blue-500 cursor-pointer text-white m-1 px-3 py-2 rounded-2xl font-medium title-text">{props.item}</a>
         )
     }
 
+    // Called on input submit (enter key). Inserts the item into the state array
     const InsertItem = () => {
         setIngredientsList([...ingredientsList, inputRef.current.value])
     }
@@ -47,7 +49,7 @@ const SearchScreen = () => {
     return (
         <div className="flex flex-col my-3 mx-5">
             {/* Ingredients */}
-            <div className="flex flex-col w-full my-10">
+            <div className="flex flex-col w-full my-5">
                 <div className="flex flex-row items-center">
                     <h1 className="text-3xl font-extrabold title-text">Ingredients</h1>
                     <span className="flex justify-center items-center text-xl font-bold rounded-xl ml-5 p-4 w-7 h-7 bg-orange-400 text-white">{ingredientsList.length}</span>
@@ -70,7 +72,7 @@ const SearchScreen = () => {
             </div>
 
             {/* Max Additional Ingredients */}
-            <div className="flex flex-col w-full my-10">
+            <div className="flex flex-col w-full my-5">
                 <div className="flex flex-row items-center">
                     <h1 className="text-3xl font-extrabold title-text">Ingredients</h1>
                     <span className="flex justify-center items-center text-xl font-bold rounded-xl ml-5 p-4 w-7 h-7 bg-orange-400 text-white">0</span>
@@ -81,7 +83,7 @@ const SearchScreen = () => {
             </div>
 
             {/* Exclude Ingredients */}
-            <div className="flex flex-col w-full my-10">
+            <div className="flex flex-col w-full my-5">
                 <div className="flex flex-row items-center">
                     <h1 className="text-3xl font-extrabold title-text">Exclude Ingredients</h1>
                     <span className="flex justify-center items-center text-xl font-bold rounded-xl ml-5 p-4 w-7 h-7 bg-orange-400 text-white">0</span>
