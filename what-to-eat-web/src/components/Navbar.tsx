31b1a4bce3405f94e6b655d560e160efcb68afad
Arvilio <100873979+Riiduu@users.noreply.github.com>
2024-10-15 13:27:24 +0300

    more top margin

--- what-to-eat-web/src/components/Navbar.tsx
+++ what-to-eat-web/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import {searchIcon, closeIcon} from '../assets/exports';
 
 const Navbar = (props: any) => {
     return (
-        <div className="flex flex-row w-full justify-between items-center h-20 px-5">
+        <div className="flex flex-row w-full justify-between mt-5 items-center h-20 px-5">
             <h1 className="text-4xl font-extrabold title-text">{props.searchActive ? 'Filters' : 'What to eat?' }</h1>
             <div onClick={props.switchToSearch} className="bg-black cursor-pointer h-12 flex w-12 rounded-xl">
                 <img src={props.searchActive ? closeIcon : searchIcon} className="h-6 m-auto" alt="" />
